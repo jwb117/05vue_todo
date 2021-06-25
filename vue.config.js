87bda1d89770845dbfd444e60b52e7b1ebcfcfa1
jwb117 <jwbjoo@gmail.com>
2021-06-25 17:37:55 +0900

    ff

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     pwa: {
       name: 'Do It!',
-      themeColor: '##00D8FF',
+      themeColor: '#00D8FF',
       msTileColor: '#ffffff',
       workboxOptions: {
         exclude: [/\.map$/, /manifest\.json$/, 'index.html']
